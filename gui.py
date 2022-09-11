4edfc5487d86f2212a53762645e5bd985f58a859
Dheeraj Vislawath <57468596+DeerEdge@users.noreply.github.com>
2022-09-11 17:04:36 -0600

    database addition

--- gui.py
+++ gui.py
@@ -7,7 +7,33 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 # folium v0.12.1 - Used to display geographical data
 import folium
 import io
+import sqlite3
 
+def readSqliteTable():
+    try:
+        sqliteConnection = sqlite3.connect('identifier.sqlite')
+        cursor = sqliteConnection.cursor()
+        print("Connected to SQLite")
+
+        sqlite_select_query = """SELECT * from events"""
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        print("Total rows are:  ", len(records))
+        print("Printing each row")
+        for row in records:
+            print("Id: ", row[0])
+
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to read data from sqlite table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+readSqliteTable()
 # SQL
 # import mysql.connector
 #
