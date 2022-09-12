862fd720700a4291fa0b293b7f95c3bc02eaec08
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-09-12 14:17:20 -0600

    More data

--- gui.py
+++ gui.py
@@ -458,7 +458,7 @@ class ui_main_window(object):
             if str(event_year) == str(events_year):
                 if str(new_month) == str(events_month):
                     if str(event_day) == str(events_day):
-                        self.admin_current_events.setText("Events on " + selected_date[4:] + ": " + event[1] + "\n" + event[3])
+                        self.admin_current_events.setText("Events on " + selected_date[4:] + ": " + event[2])
 
     def student_upcoming_events_calendar(self):
         selected_date = self.upcoming_events_tab.sender().selectedDate().toString()
@@ -501,7 +501,7 @@ class ui_main_window(object):
             if str(event_year) == str(events_year):
                 if str(new_month) == str(events_month):
                     if str(event_day) == str(events_day):
-                        self.day_events.setText("Events on " + selected_date[4:] + ": " + event[1])
+                        self.day_events.setText("Events on " + selected_date[4:] + ": " + event[2])
 
 
 
