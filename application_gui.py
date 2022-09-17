bb0859d99498de2386145db539c1947a1efe0d6c
Hyun-Dai <hyunchun19@gmail.com>
2022-09-17 14:13:08 -0600

    points

--- application_gui.py
+++ application_gui.py
@@ -277,35 +277,35 @@ class ui_main_window(object):
         self.points_title_line = self.create_QFrame("points_tab", "points_title_line", "HLine", 10, 65, 600, 6)
         self.personal_points_label = self.create_QLabel("points_tab", "personal_points_label", "  Personal Points", 20, 80, 300, 30)
         self.personal_points = self.create_QLineEdit("points_tab", "personal_points", True, 20, 110, 300, 300)
+        self.points_leaderboard_objects = self.create_QScrollArea("points_tab", "points_leaderboard_QScrollArea",
+                                                                  "vertical_layout", 350, 110, 450, 300)
+        self.points_leaderboard = self.points_leaderboard_objects[0]
+        self.points_leaderboard_layout = self.points_leaderboard_objects[1]
+        self.points_leaderboard_scrollArea = self.points_leaderboard_objects[2]
         self.points_leaderboard_label = self.create_QLabel("points_tab", "points_leaderboard_label", "  Leaderboard", 350, 80, 450, 30)
-        self.points_leaderboard = self.create_QLineEdit("points_tab", "point_leaderboard", True, 350, 110, 450, 300)
 
         # Sort Students By Points
         sqliteConnection = sqlite3.connect('identifier.sqlite')
         cursor = sqliteConnection.cursor()
         cursor.execute("SELECT FIRST_NAME, LAST_NAME, POINTS FROM students")
         student_rows = cursor.fetchall()
+
         def sort_key(student_rows):
             return student_rows[2]
+
         student_rows.sort(key=sort_key, reverse=True)
         cursor.close()
 
-        self.tableWidget = QTableWidget()
-
-        # Row count
-        self.tableWidget.setRowCount(20)
-
-        # Column count
-        self.tableWidget.setColumnCount(2)
-
-        self.tableWidget.setItem(0, 0, QTableWidgetItem("Name"))
-        self.tableWidget.setItem(0, 1, QTableWidgetItem("Points"))
-        for a, b, c in student_rows:
-            a, b, c = zip(*student_rows)
-            d = list(zip(b, a))
-            self.tableWidget.setItem(20, 0, QTableWidgetItem(str(d)))
-            self.tableWidget.setItem(20, 1, QTableWidgetItem(str(c)))
-
+        # Leaderboard
+        for i in range(20):
+            self.event_object = QtWidgets.QGroupBox(self.points_leaderboard)
+            self.event_object.setFixedSize(400, 50)
+            self.event_object.setLayout(QtWidgets.QVBoxLayout())
+            self.label = self.create_QLabel("event", "test", "   Points",
+                                            0, 0, 100, 30)
+            self.points_leaderboard_layout.addWidget(self.event_object)
+        self.points_leaderboard_scrollArea.setWidget(self.points_leaderboard)
+        self.points_leaderboard_scrollArea.verticalScrollBar().setSliderPosition(0)
 
         # Rewards Tab
         sqliteConnection = sqlite3.connect('identifier.sqlite')
@@ -701,6 +701,8 @@ class ui_main_window(object):
             self.QScrollArea = QtWidgets.QScrollArea(self.dashboard_tab)
         elif container == "maps_tab":
             self.QScrollArea = QtWidgets.QScrollArea(self.maps_tab)
+        elif container == "points_tab":
+            self.QScrollArea = QtWidgets.QScrollArea(self.points_tab)
         elif container == "rewards_tab":
             self.QScrollArea = QtWidgets.QScrollArea(self.rewards_tab)
         self.QScrollArea.setFixedWidth(fixed_width)
