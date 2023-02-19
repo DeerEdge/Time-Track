4615bfc75484fcc7780cc91682ab4bac4979383b
AdhuBavu2 <adhu.ramki@gmail.com>
2023-02-18 23:02:57 -0700

    repush

--- user_details.py
+++ user_details.py
@@ -4,10 +4,10 @@ class get_user_details():
         self.last_name = self.logged_in_user_details[0][2]
         self.date_of_birth = self.logged_in_user_details[0][8]
         self.user_gender = self.logged_in_user_details[0][16]
-        self.grade = str(self.logged_in_user_details[0][7])
-        self.events_attended = str(self.logged_in_user_details[0][10])
-        self.user_points = str(self.logged_in_user_details[0][11])
-        self.user_profile_picture = str(self.logged_in_user_details[0][12])
+        self.grade = self.logged_in_user_details[0][7]
+        self.events_attended = self.logged_in_user_details[0][10]
+        self.user_points = self.logged_in_user_details[0][11]
+        self.user_profile_picture = self.logged_in_user_details[0][12]
         self.emergency_contact_name = self.logged_in_user_details[0][13]
         self.emergency_contact_phone = self.logged_in_user_details[0][14]
         self.emergency_contact_email = self.logged_in_user_details[0][15]
